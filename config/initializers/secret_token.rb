8d178e2dcfa8e33ddec377188e62a718ad22b9d7
Sakif Imtiaz <sakif@umich.edu>
2013-05-08 20:30:47 -0400

    intitial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hopelauncher::Application.config.secret_token = 'e691cba49cdb9d649cc69cd69fd42b01fdee7ccd3433f7babf8ef2129a3079b26c35ff32dc3551a51f882d26771f14ddbc3c9dd9581adf19b523ad7238b9c68b'
