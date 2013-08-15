c0ee0eb8496297d2d2ab1d89aef2a83c0cb0288b
Sakif Imtiaz <sakif@umich.edu>
2013-08-15 09:52:51 -0400

    changed host in production again

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,7 @@
 Hopelauncher::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
-  config.action_mailer.default_url_options = { :host => 'mail.hopelauncher.com' }
+  config.action_mailer.default_url_options = { :host => 'hopelauncher.com' }
   # Code is not reloaded between requests
   config.cache_classes = true
 
