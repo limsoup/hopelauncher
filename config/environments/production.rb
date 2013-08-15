472b5471c3e648bbfb89ea1f6003e976d07538a9
Sakif Imtiaz <sakif@umich.edu>
2013-08-15 09:57:08 -0400

    changed added some other settings from stackoverflow.com/questions/18209006/heroku-sendgrid-actionmailer-errnoeconnrefused-connection-refused-con

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -56,8 +56,10 @@ Hopelauncher::Application.configure do
   # config.assets.precompile += %w( search.js )
 
   # Disable delivery errors, bad email addresses will be ignored
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = false
 
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
   # Enable threaded mode
   # config.threadsafe!
 
