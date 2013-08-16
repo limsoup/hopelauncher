38ff96f55469302de053ea55c70ca02d436672e7
Sakif Imtiaz <sakif@umich.edu>
2013-08-16 08:34:17 -0400

    changed cancan logging option

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   	render '/layouts/facebook/channel'
   end
   rescue_from CanCan::AccessDenied do |exception|
-	Rails.logger.debug "Access denied on #{exception.action} #{exception.subject.inspect}"
+	Rails.logger "Access denied on #{exception.action} #{exception.subject.inspect}"
   end
-  
+
 end
