fe48a157e79bd39889e2fd44e99a4c56d8989836
Sakif Imtiaz <sakif@umich.edu>
2013-10-08 19:25:04 -0400

    new migration to rename tos to terms of service

--- db/migrate/20131008232340_rename_tos_accept_to_terms_of_service.rb
+++ db/migrate/20131008232340_rename_tos_accept_to_terms_of_service.rb
@@ -1,4 +1,4 @@
-class ChangeTermsOfService < ActiveRecord::Migration
+class RenameTosAcceptToTermsOfService < ActiveRecord::Migration
   def change
   	rename_column :users, :tos_accept, :terms_of_service
   end
