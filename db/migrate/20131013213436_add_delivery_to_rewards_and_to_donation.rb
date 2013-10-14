cdd23fcb6894c87776e6fc7f609573716acf57f2
Sakif Imtiaz <sakif@umich.edu>
2013-10-14 00:27:59 -0400

    fixing migrations

--- db/migrate/20131013213436_add_delivery_to_rewards_and_to_donation.rb
+++ db/migrate/20131013213436_add_delivery_to_rewards_and_to_donation.rb
@@ -1,6 +1,6 @@
 class AddDeliveryToRewardsAndToDonation < ActiveRecord::Migration
   def change
-  	add_column :donations, :delivery_address, :hash
+  	add_column :donations, :delivery_address, :string
   	add_column :rewards, :delivery, :string, default: "none"
   end
 end
